42e47a82cd23145d33112ce18cf10d2d1214a8bc
lyc <280369571@qq.com>
2022-06-13 20:11:50 +0800

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,8 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent';
-import Home from './HomeComponent'
+import Home from './HomeComponent';
+import About  from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -43,6 +44,12 @@ class Main extends Component {
       );
     }
 
+    const AboutUs = () => {
+      return(
+        <About leaders={this.state.leaders}/>
+      );
+    }
+
     return (
       <div>
         <Header />
@@ -50,6 +57,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes = {this.state.dishes}/>} />
           <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/aboutus" component={AboutUs} />
           <Route exact path="/contactus" component={Contact}/>
           <Redirect to="/home" />
         </Switch>
